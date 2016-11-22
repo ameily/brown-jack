75dd3c4f6437cddd7752f5b4cb7444c02cfeeb66
Adam Meily <meilya@ainfosec.com>
2016-11-21 22:10:08 -0500

    initial commit of configurable logger

---
+++ log.js
@@ -0,0 +1,18 @@
+
+var colors = require('colors/safe');
+var moment = require('moment');
+
+function makeLogger(name, color) {
+  return function() {
+    var prefix = "";
+    color = color || colors.green;
+    prefix = colors.gray('[' + moment().format("HH:mm:ss") + ']') + ' ' + color(name) + ' ';
+
+    arguments[0] = prefix + arguments[0];
+    console.log.apply(console, arguments);
+  };
+}
+
+
+module.exports = makeLogger;
+
